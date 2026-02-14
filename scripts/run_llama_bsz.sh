3dbfbc108539dff90c8bda01e043cf3da0066f9e
Phuc3010 <marksmans2002@gmail.com>
2026-02-14 04:14:45 +0100

    Add training scripts

--- scripts/run_llama_bsz.sh
+++ scripts/run_llama_bsz.sh
@@ -9,28 +9,25 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH -G H100:4
 
+export VLLM_DISABLE_COMPILE_CACHE=1
 source ~/.bashrc
 conda activate prm_rlvr
-export VLLM_DISABLE_COMPILE_CACHE=1
-# bszs=(16 32 64 128 256 512)
 
-model_names=("Llama-3.2-3B-Instruct" "Qwen2.5-3B-Instruct")
-rollout_szs=(2 4 8 16 32)
-total_bsz=512
+bszs=(16 32 64 128 256 512)
+rollout_sz=16
 temp=1.0
 
-for model_name in "${model_names[@]}";do
-for rollout_sz in "${rollout_szs[@]}";do
+for bsz in "${bszs[@]}";do
+prompt_bsz=$(( bsz / rollout_sz ))
 echo "Training with prompt batch size $prompt_bsz"
-rm -rf ~/.cache
-prompt_bsz=$(( total_bsz / rollout_sz))
 
+rm -rf ~/.cache
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     algorithm.norm_adv_by_std_in_grpo=false\
     data.train_files=data/polaris/train.parquet \
     data.val_files=['data/math/test.parquet']\
-    data.train_batch_size=16 \
+    data.train_batch_size=${prompt_bsz} \
     data.max_prompt_length=1024 \
     data.max_response_length=3072\
     data.filter_overlong_prompts=True \
@@ -38,11 +35,11 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.ref.strategy=fsdp2 \
     actor_rollout_ref.actor.strategy=fsdp2 \
     data.truncation=right \
-    actor_rollout_ref.model.path=models/${model_name} \
+    actor_rollout_ref.model.path=models/Llama-3.2-3B-Instruct \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=true \
     actor_rollout_ref.actor.use_dynamic_bsz=true \
-    actor_rollout_ref.actor.ppo_mini_batch_size=64 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=${prompt_bsz} \
     actor_rollout_ref.actor.use_kl_loss=False \
     actor_rollout_ref.actor.kl_loss_coef=0.0 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
@@ -54,14 +51,13 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.7 \
-    actor_rollout_ref.rollout.n=${rollout_sz}\
-    actor_rollout_ref.rollout.temperature=$temp\
+    actor_rollout_ref.rollout.n=16\
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.use_kl_in_reward=False \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb']\
     trainer.project_name='RLVR-Peft' \
-    trainer.experiment_name=${model_name}-polaris-GRPO-rollout_sz${rollout_sz}\
+    trainer.experiment_name=Llama-3.2-3B-Instruct-polaris-GRPO--bsz${bsz}\
     reward_model.reward_manager=deepscaler \
     trainer.n_gpus_per_node=4 \
     trainer.val_before_train=true \
